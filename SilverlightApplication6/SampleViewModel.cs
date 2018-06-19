f94c7d17dea0bb634e84fc7966cea16c81952962
User01 <User01@DESKTOP-PGAHMIC>
2018-06-19 05:29:43 -0400

    Add project files.

---
+++ SilverlightApplication6/SampleViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace SilverlightApplication6
+{
+    public class SampleViewModel
+    {
+        public DataModelCollection Data { get; set; }
+
+        public SampleViewModel()
+        {
+            Data = new DataModelCollection {
+                new DataModel { NodeName = "Node 11", Children = new DataModelCollection {
+                    new DataModel { NodeName = "Node 113" },
+                    new DataModel { NodeName = "Node 123" },
+                    new DataModel { NodeName = "Node 133" },
+                } },
+                new DataModel { NodeName = "Node 22", Children = new DataModelCollection {
+                    new DataModel { NodeName = "Node 213" },
+                    new DataModel { NodeName = "Node 223" },
+                } },
+                new DataModel { NodeName = "Node 33", Children = new DataModelCollection {
+                    new DataModel { NodeName = "Node 313" },
+                } },
+            };
+        }
+    }
+}
---
+++ SilverlightApplication6/SilverlightControl1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightApplication6
+{
+    public partial class SilverlightControl1 : UserControl
+    {
+        SampleViewModel vm;
+
+        public SilverlightControl1()
+        {
+            InitializeComponent();
+
+            vm = new SampleViewModel();
+            DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataModel dm = (DataModel)trv1.SelectedItem;
+
+            if (dm == null)
+                vm.Data.Add(new DataModel { NodeName = txt1.Text });
+            else dm.Children.Add(new DataModel { NodeName = txt1.Text });
+        }
+    }
+}
